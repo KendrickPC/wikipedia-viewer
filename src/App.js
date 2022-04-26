cd7426a52e9ceab04700037abdfe5f31b62e464c
KendrickPC <kennethpchang@gmail.com>
2022-04-26 15:42:52 -0700

    title and snippet loaded into view

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import {useState} from 'react';
+
 function App() {
+  const [search, setSearch] = useState("")
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async e => {
+    e.preventDefault();
+    if (search === '') return;
+
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+
+    const response = await fetch(endpoint);
+    // console.log(response);
+    if (response.ok === false) {
+      throw Error(response.statusText)
+    }
+    const json = await response.json()
+    // console.log(json);
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo)
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Wikipedia Seeker</h1>
-        <form className="search-box">
-          <input type="search" placeholder="What are you looking for?" />
+        <form className="search-box" onSubmit={handleSearch}>
+          <input 
+            type="search"
+            placeholder="What are you looking for?"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
           
         </form>
-        <p>Search Results: 0</p>
+        { (searchInfo.totalhits) ? <p>Search Results: {searchInfo.totalhits}</p> : ''}
       </header>
       <div className="results">
-        <div className="result">
-          <h3>Title Goes Here</h3>
-          <p>
-            Lorem Ipsum dolor sit amet
-          </p>
-          <a href="#">Read More</a>
-        </div>
+        {results.map( (result, i) => {
+          return (
+            <div className="result" key={i}>
+            <h3>{result.title}</h3>
+            <p>
+              {result.snippet}
+            </p>
+            <a href="#">Read More</a>
+          </div>
+          )
+        })}
+
       </div>
     </div>
   );
